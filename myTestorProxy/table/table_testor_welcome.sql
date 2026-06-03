8ecd52a9fb99dc38c5fe65a9497b0e4c2521dab1
[ progorker ] Software Development as a Platform <zinospetrel@sdf.org>
2026-06-03 16:43:46 +0700

    Add files via upload

--- myTestorProxy/table/table_testor_welcome.sql
+++ myTestorProxy/table/table_testor_welcome.sql
@@ -17,7 +17,7 @@ create table testor_welcome (
 
 insert into testor_welcome ( `message` ) values ( 'Welcome to myTestor 0.0.1!' );
 
-insert into testor_welcome ( `message` ) values ( 'There are 38 published API procedures: ' );
+insert into testor_welcome ( `message` ) values ( 'There are 39 published API procedures: ' );
 
 insert into testor_welcome ( `message` ) values ( 'proc: api_testor_current_user' );
 insert into testor_welcome ( `message` ) values ( 'proc: api_testor_login' );
@@ -57,4 +57,5 @@ insert into testor_welcome ( `message` ) values ( 'proc: api_testor_not_contains
 insert into testor_welcome ( `message` ) values ( 'proc: api_testor_has_right' );
 insert into testor_welcome ( `message` ) values ( 'proc: api_testor_is_online' );
 insert into testor_welcome ( `message` ) values ( 'proc: api_testor_man' );
+insert into testor_welcome ( `message` ) values ( 'proc: api_testor_pattern' );
 
